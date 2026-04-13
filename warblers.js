99cfc1ccd1e0af0cbaffac1e26520eedbd5276c4
anramb <anramb@gmail.com>
2026-04-13 21:47:12 +0200

    warblers.js

--- warblers.js
+++ warblers.js
@@ -1,15 +1,15 @@
 const birds_waders = [
   
 {english: "Olive-tree Warbler", afrikaans: "Olyfboomsanger", hotspot: "Zaagkuildrift", habitat: "Bushveld", birdgroup: "Warblers", level: "3 Advanced", 
- audio: "XC819380_Olive-treeWarbler.mp3", spectrogram: "XC819380_Olive-treeWarbler.png",credit: "© Rick Nuttall, www.xeno-canto.org, XC819380", licenseLink: "https://creativecommons.org/licenses/by-nc-sa/4.0/", changes: "Original recording"},
+ audio: "warblers/XC819380_Olive-treeWarbler.mp3", spectrogram: "warblers/XC819380_Olive-treeWarbler.png",credit: "© Rick Nuttall, www.xeno-canto.org, XC819380", licenseLink: "https://creativecommons.org/licenses/by-nc-sa/4.0/", changes: "Original recording"},
 
 {english: "Common Whitethroat alarm call", afrikaans: "Witkeelsanger alarm call", hotspot: "Zaagkuildrift", habitat: "Bushveld", birdgroup: "Warblers", level: "3 Advanced", 
- audio: "XC521207_CommonWhitethroat.mp3", spectrogram: "XC521207_CommonWhitethroat.png",credit: "© Tony Archer, www.xeno-canto.org, XC521207, Alarm call, Background birds: red-faced mousebird", licenseLink: "https://creativecommons.org/licenses/by-nc-sa/4.0/", changes: "Original recording"},
+ audio: "warblers/XC521207_CommonWhitethroat.mp3", spectrogram: "warblers/XC521207_CommonWhitethroat.png",credit: "© Tony Archer, www.xeno-canto.org, XC521207, Alarm call, Background birds: red-faced mousebird", licenseLink: "https://creativecommons.org/licenses/by-nc-sa/4.0/", changes: "Original recording"},
 
 {english: "Barred Wren-Warbler", afrikaans: "Gebande Sanger", hotspot: "Zaagkuildrift", habitat: "Bushveld", birdgroup: "Warblers", level: "2 Intermediate", 
- audio: "XC708257_BarredWren-Warbler.mp3", spectrogram: "XC708257_BarredWren-Warbler.png",credit: "© Tony Archer, www.xeno-canto.org, XC708257", licenseLink: "https://creativecommons.org/licenses/by-nc-sa/4.0/", changes: "Original recording"},
+ audio: "warblers/XC708257_BarredWren-Warbler.mp3", spectrogram: "warblers/XC708257_BarredWren-Warbler.png",credit: "© Tony Archer, www.xeno-canto.org, XC708257", licenseLink: "https://creativecommons.org/licenses/by-nc-sa/4.0/", changes: "Original recording"},
 
   {english: "Marsh Warbler alarm call", afrikaans: "Europese Rietsanger alarm call", hotspot: "Zaagkuildrift", habitat: "Bushveld", birdgroup: "Warblers", level: "3 Advanced", 
-   audio: "XC632299_MarshWarbler.mp3", spectrogram: "XC632299_MarshWarbler.png",credit: "© Tony Archer, www.xeno-canto.org, XC632299; Alarm call", licenseLink: "https://creativecommons.org/licenses/by-nc-sa/4.0/", changes: "Original recording"},
+   audio: "warblers/XC632299_MarshWarbler.mp3", spectrogram: "warblers/XC632299_MarshWarbler.png",credit: "© Tony Archer, www.xeno-canto.org, XC632299; Alarm call", licenseLink: "https://creativecommons.org/licenses/by-nc-sa/4.0/", changes: "Original recording"},
   
 ];
