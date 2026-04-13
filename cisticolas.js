ddb0b22b9708c93c1c6194833026319203f2d836
anramb <anramb@gmail.com>
2026-04-13 21:42:59 +0200

    cisticolas.js

--- cisticolas.js
+++ cisticolas.js
@@ -1,10 +1,10 @@
 const birds_cisticolas = [
   
 {english: "Chirping Cisticola", afrikaans: "Piepende Tinktinkie", hotspot: "Caprivi River System", habitat: "Riverine shrubs", birdgroup: "Cisticola", level: "3 Advanced",
- audio: "XC339101_ChirpingCisticola.mp3", spectrogram: "XC339101_ChirpingCisticola.png",credit: "© Peter Boesman, www.xeno-canto.org, XC339101", licenseLink: "https://creativecommons.org/licenses/by-nc-nd/4.0/", changes: "Original recording"},
+ audio: "cisticolas/XC339101_ChirpingCisticola.mp3", spectrogram: "cisticolas/XC339101_ChirpingCisticola.png",credit: "© Peter Boesman, www.xeno-canto.org, XC339101", licenseLink: "https://creativecommons.org/licenses/by-nc-nd/4.0/", changes: "Original recording"},
 
 {english: "Luapula Cisticola", afrikaans: "Luapulatinktinkie", hotspot: "Caprivi River System", habitat: "Grassland", birdgroup: "Cisticola", level: "3 Advanced",
- audio: "XC978973_LuapulaCisticola.mp3", spectrogram: "XC978973_LuapulaCisticola.png",credit: "© Dries Van de Loock, www.xeno-canto.org, XC978973", licenseLink: "https://creativecommons.org/licenses/by-nc-nd/4.0/", changes: "Original recording"},
+ audio: "cisticolas/XC978973_LuapulaCisticola.mp3", spectrogram: "cisticolas/XC978973_LuapulaCisticola.png",credit: "© Dries Van de Loock, www.xeno-canto.org, XC978973", licenseLink: "https://creativecommons.org/licenses/by-nc-nd/4.0/", changes: "Original recording"},
 
 
 ];
