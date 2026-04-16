af850cd66c112afbc616e22ce3a7d0718176f21f
anramb <anramb@gmail.com>
2026-04-16 19:26:43 +0200

    LBJs.js

--- LBJs.js
+++ LBJs.js
@@ -1,10 +1,5 @@
 const LBJs = [
   
-{english: "Chirping Cisticola", afrikaans: "Piepende Tinktinkie", hotspot: "Caprivi River System", habitat: "Riverine shrubs", birdgroup: "Cisticola", level: "3 Advanced",
- audio: "cisticolas/XC339101_ChirpingCisticola.mp3", spectrogram: "cisticolas/XC339101_ChirpingCisticola.png",credit: "© Peter Boesman, www.xeno-canto.org, XC339101", licenseLink: "https://creativecommons.org/licenses/by-nc-nd/4.0/", changes: "Original recording"},
-
-{english: "Luapula Cisticola", afrikaans: "Luapulatinktinkie", hotspot: "Caprivi River System", habitat: "Grassland", birdgroup: "Cisticola", level: "3 Advanced",
- audio: "cisticolas/XC978973_LuapulaCisticola.mp3", spectrogram: "cisticolas/XC978973_LuapulaCisticola.png",credit: "© Dries Van de Loock, www.xeno-canto.org, XC978973", licenseLink: "https://creativecommons.org/licenses/by-nc-nd/4.0/", changes: "Original recording"},
-
-
+{english: "Rufous-naped lark", afrikaans: "Rooineklewerik", hotspot: "Gauteng; KZN; Limpopo; Free State; Northwest; Northern Cape; Eastern Cape", habitat: "Grassland", birdgroup: "LBJs", level: "1 Beginner", 
+ audio: "LBJs/XC1100388_Rufous-naped Lark", spectrogram: "LBJs/XC1100388_Rufous-naped Lark",credit: "© Marna Buys, www.xeno-canto.org, XC1100388", licenseLink: "https://creativecommons.org/licenses/by-nc-sa/4.0/", changes: "Original recording"}
 ];
