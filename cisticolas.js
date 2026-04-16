5e58953db6befa01f9215050d807a4f8c7b7b9b2
anramb <anramb@gmail.com>
2026-04-16 22:07:00 +0200

    cisticolas.js

--- cisticolas.js
+++ cisticolas.js
@@ -5,5 +5,5 @@ const cisticolas = [
  audio: "cisticolas/XC1100387_RattlingCisticola.mp3", spectrogram: "cisticolas/XC1100387_RattlingCisticola.png",image: "cisticolas/XC1100387_RattlingCisticola.jpg", credit: "© Marna Buys, www.xeno-canto.org, XC1100387", licenseLink: "https://creativecommons.org/licenses/by-nc-sa/4.0/", changes: "Original recording"},
 
 {english: "Chirping Cisticola", afrikaans: "Piepende Tinktinkie", hotspot: "Caprivi River System", habitat: "Riverine shrubs", birdgroup: "Cisticolas", level: "3 Advanced",
-audio: "cisticolas/XC339101_ChirpingCisticola.mp3", spectrogram: "cisticolas/XC339101_ChirpingCisticola.png",image: "cisticolas/XC339101_ChirpingCisticola.jpg", credit: "© Peter Boesman, www.xeno-canto.org, XC339101", licenseLink: "https://creativecommons.org/licenses/by-nc-nd/4.0/", changes: "Original recording"},
+audio: "cisticolas/XC339101_ChirpingCisticola.mp3", spectrogram: "cisticolas/XC339101_ChirpingCisticola.png",image: "cisticolas/XC339101_ChirpingCisticola.jpg", credit: "© Peter Boesman, www.xeno-canto.org, XC339101", licenseLink: "https://creativecommons.org/licenses/by-nc-nd/4.0/", changes: "Original recording"}
 ];
