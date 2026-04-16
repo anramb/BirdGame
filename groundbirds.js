6c730fb00fdb8f3d53565f568c5fe1c48cfac897
anramb <anramb@gmail.com>
2026-04-16 19:08:19 +0200

    groundbirds.js

--- groundbirds.js
+++ groundbirds.js
@@ -1,10 +1,6 @@
 const groundbirds = [
   
-{english: "Chirping Cisticola", afrikaans: "Piepende Tinktinkie", hotspot: "Caprivi River System", habitat: "Riverine shrubs", birdgroup: "Cisticola", level: "3 Advanced",
- audio: "cisticolas/XC339101_ChirpingCisticola.mp3", spectrogram: "cisticolas/XC339101_ChirpingCisticola.png",credit: "© Peter Boesman, www.xeno-canto.org, XC339101", licenseLink: "https://creativecommons.org/licenses/by-nc-nd/4.0/", changes: "Original recording"},
-
-{english: "Luapula Cisticola", afrikaans: "Luapulatinktinkie", hotspot: "Caprivi River System", habitat: "Grassland", birdgroup: "Cisticola", level: "3 Advanced",
- audio: "cisticolas/XC978973_LuapulaCisticola.mp3", spectrogram: "cisticolas/XC978973_LuapulaCisticola.png",credit: "© Dries Van de Loock, www.xeno-canto.org, XC978973", licenseLink: "https://creativecommons.org/licenses/by-nc-nd/4.0/", changes: "Original recording"},
-
+{english: "Natal Spurfowl", afrikaans: "Natalse Fisant", hotspot: "Gauteng; KZN; Limpopo; Free State; Northwest", habitat: "Woodland; Savanna", birdgroup: "Groundbirds", level: "1 Beginner", 
+ audio: "groundbirds/XC875678_NatalSpurfowl.mp3", spectrogram: "groundbirds/XC875678_NatalSpurfowl.png",credit: "© Marc Anderson, www.xeno-canto.org, XC875678", licenseLink: "https://creativecommons.org/licenses/by-nc-nd/4.0/", changes: "Original recording"},
 
 ];
