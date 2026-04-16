651f09c92125bb138824c8a63b314746df3942c7
anramb <anramb@gmail.com>
2026-04-16 19:41:29 +0200

    cisticolas.js

--- cisticolas.js
+++ cisticolas.js
@@ -7,6 +7,6 @@ const cisticolas = [
  audio: "cisticolas/XC978973_LuapulaCisticola.mp3", spectrogram: "cisticolas/XC978973_LuapulaCisticola.png",credit: "© Dries Van de Loock, www.xeno-canto.org, XC978973", licenseLink: "https://creativecommons.org/licenses/by-nc-nd/4.0/", changes: "Original recording"},
 
 {english: "Rattling Cisticola", afrikaans: "Bosveldtinktinkie", hotspot: "Gauteng; KZN; Limpopo; Free State; Northwest; Northern Cape", habitat: "Woodland", birdgroup: "Cisticola", level: "1 Beginner", 
- audio: "cisticola/XC1100387_RattlingCisticola", spectrogram: "cisticola/XC1100387_RattlingCisticola",credit: "© Marna Buys, www.xeno-canto.org, XC1100387", licenseLink: "https://creativecommons.org/licenses/by-nc-sa/4.0/", changes: "Original recording"}
+ audio: "cisticola/XC1100387_RattlingCisticola", spectrogram: "cisticola/XC1100387_RattlingCisticola",image: "cisticola/XC1100387_RattlingCisticola", credit: "© Marna Buys, www.xeno-canto.org, XC1100387", licenseLink: "https://creativecommons.org/licenses/by-nc-sa/4.0/", changes: "Original recording"}
   
 ];
