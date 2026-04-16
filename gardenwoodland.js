c74e6910c15e26f5512f7007973ad3e6f0c79d37
anramb <anramb@gmail.com>
2026-04-16 18:31:31 +0200

    Add files via upload

---
+++ gardenwoodland.js
@@ -0,0 +1,8 @@
+const gardenwoodland = [
+  
+{english: "Gorgeous Bushshrike", afrikaans: "Konkoit", hotspot: "St Lucia; Kruger National Park; Limpopo; KZN; Mpumalanga", habitat: "Woodland; Forest", birdgroup: "Garden and Woodland", level: "1 Beginner", audio: "gardenwoodland/XC1100369_GorgeousBushshrike.mp3", spectrogram: "gardenwoodland/XC1100369_GorgeousBushshrike.png",credit: "© Marna Buys, www.xeno-canto.org, XC1100369, Background birds: emerald-spotted wood dove, grey-headed bushshrike, ring-necked dove, etc", licenseLink: "https://creativecommons.org/licenses/by-nc-sa/4.0/", changes: "Original recording"}
+
+
+
+
+];
