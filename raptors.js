5f8d2e450b4a3a9472dfac5526ebb43ec883b867
anramb <anramb@gmail.com>
2026-04-16 20:05:53 +0200

    raptors.js

--- raptors.js
+++ raptors.js
@@ -1,10 +1,6 @@
 const raptors = [
   
-{english: "Chirping Cisticola", afrikaans: "Piepende Tinktinkie", hotspot: "Caprivi River System", habitat: "Riverine shrubs", birdgroup: "Cisticola", level: "3 Advanced",
- audio: "cisticolas/XC339101_ChirpingCisticola.mp3", spectrogram: "cisticolas/XC339101_ChirpingCisticola.png",credit: "© Peter Boesman, www.xeno-canto.org, XC339101", licenseLink: "https://creativecommons.org/licenses/by-nc-nd/4.0/", changes: "Original recording"},
-
-{english: "Luapula Cisticola", afrikaans: "Luapulatinktinkie", hotspot: "Caprivi River System", habitat: "Grassland", birdgroup: "Cisticola", level: "3 Advanced",
- audio: "cisticolas/XC978973_LuapulaCisticola.mp3", spectrogram: "cisticolas/XC978973_LuapulaCisticola.png",credit: "© Dries Van de Loock, www.xeno-canto.org, XC978973", licenseLink: "https://creativecommons.org/licenses/by-nc-nd/4.0/", changes: "Original recording"},
-
+{english: "Gabar Goshawk", afrikaans: "Witkruissperwer", hotspot: "Gauteng; Limpopo; Mpumalanga; KZN; Northwest; Karoo", habitat: "Woodland; Grassland", birdgroup: "Raptors", level: "1 Beginner", 
+ audio: "raptors/XC1100393_GabarGoshawk.mp3", spectrogram: "raptors/XC1100393_GabarGoshawk.png",image: "raptors/XC1100393_GabarGoshawk.jpg", credit: "© Marna Buys, www.xeno-canto.org, XC1100393", licenseLink: "https://creativecommons.org/licenses/by-nc-sa/4.0/", changes: "Original recording"}
 
 ];
