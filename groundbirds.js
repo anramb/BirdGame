fc70ae9735cc02e6dc31ba6f7ce98adfa0c4000f
anramb <anramb@gmail.com>
2026-04-16 19:42:59 +0200

    groundbirds.js

--- groundbirds.js
+++ groundbirds.js
@@ -1,6 +1,5 @@
 const groundbirds = [
   
-{english: "Natal Spurfowl", afrikaans: "Natalse Fisant", hotspot: "Gauteng; KZN; Limpopo; Free State; Northwest", habitat: "Woodland; Savanna", birdgroup: "Groundbirds", level: "1 Beginner", 
- audio: "groundbirds/XC875678_NatalSpurfowl.mp3", spectrogram: "groundbirds/XC875678_NatalSpurfowl.png",credit: "© Marc Anderson, www.xeno-canto.org, XC875678", licenseLink: "https://creativecommons.org/licenses/by-nc-nd/4.0/", changes: "Original recording"},
-
+{english: "Natal Spurfowl", afrikaans: "Natalse Fisant", hotspot: "Gauteng; KZN; Limpopo; Free State; Northwest", habitat: "Woodland; Grassland", birdgroup: "Groundbirds", level: "1 Beginner", 
+ audio: "groundbirds/XC875678_NatalSpurfowl.mp3", spectrogram: "groundbirds/XC875678_NatalSpurfowl.png",image: "groundbirds/XC875678_NatalSpurfowl.jpg", credit: "© Marc Anderson, www.xeno-canto.org, XC875678", licenseLink: "https://creativecommons.org/licenses/by-nc-nd/4.0/", changes: "Original recording"}
 ];
