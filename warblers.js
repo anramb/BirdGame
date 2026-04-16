4653f95ae3dcef2426bc790720d9716718cedbe5
anramb <anramb@gmail.com>
2026-04-16 16:40:19 +0200

    warblers.js

--- warblers.js
+++ warblers.js
@@ -9,7 +9,10 @@ const warblers = [
 {english: "Barred Wren-Warbler", afrikaans: "Gebande Sanger", hotspot: "Zaagkuildrift", habitat: "Bushveld", birdgroup: "Warblers", level: "2 Intermediate", 
  audio: "warblers/XC708257_BarredWren-Warbler.mp3", spectrogram: "warblers/XC708257_BarredWren-Warbler.png",credit: "© Tony Archer, www.xeno-canto.org, XC708257", licenseLink: "https://creativecommons.org/licenses/by-nc-sa/4.0/", changes: "Original recording"},
 
-  {english: "Marsh Warbler alarm call", afrikaans: "Europese Rietsanger alarm call", hotspot: "Zaagkuildrift", habitat: "Bushveld", birdgroup: "Warblers", level: "3 Advanced", 
-   audio: "warblers/XC632299_MarshWarbler.mp3", spectrogram: "warblers/XC632299_MarshWarbler.png",credit: "© Tony Archer, www.xeno-canto.org, XC632299; Alarm call", licenseLink: "https://creativecommons.org/licenses/by-nc-sa/4.0/", changes: "Original recording"},
+{english: "Rudd's Apalis", afrikaans: "Ruddse Kleinjantjie", hotspot: "St Lucia", habitat: "Forest", birdgroup: "Warblers", level: "3 Advanced", 
+ audio: "warblers/XC62390_RuddsApalis.mp3", spectrogram: "warblers/XC62390_RuddsApalis.png",photo: "warblers/XC62390_RuddsApalis.jpg", credit: "© Charles Hesse, www.xeno-canto.org, XC62390", licenseLink: "https://creativecommons.org/licenses/by-nc-nd/2.5/", changes: "Original recording"},
+  
+{english: "Marsh Warbler alarm call", afrikaans: "Europese Rietsanger alarm call", hotspot: "Zaagkuildrift", habitat: "Bushveld", birdgroup: "Warblers", level: "3 Advanced", 
+audio: "warblers/XC632299_MarshWarbler.mp3", spectrogram: "warblers/XC632299_MarshWarbler.png",credit: "© Tony Archer, www.xeno-canto.org, XC632299; Alarm call", licenseLink: "https://creativecommons.org/licenses/by-nc-sa/4.0/", changes: "Original recording"},
   
 ];
