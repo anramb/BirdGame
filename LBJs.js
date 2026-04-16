84273085a1949a7014ba9d0cd5b7057b6393407e
anramb <anramb@gmail.com>
2026-04-16 21:38:24 +0200

    LBJs.js

--- LBJs.js
+++ LBJs.js
@@ -1,6 +1,6 @@
 const LBJs = [
   
-{english: "Rufous-naped lark", afrikaans: "Rooineklewerik", hotspot: "Gauteng; KZN; Limpopo; Free State; Northwest; Northern Cape; Eastern Cape", habitat: "Grassland", birdgroup: "LBJs", level: "1 Beginner", 
- audio: "LBJs/XC1100388_Rufous-napedLark", spectrogram: "LBJs/XC1100388_Rufous-napedLark",image: "LBJs/XC1100388_Rufous-napedLark", credit: "© Marna Buys, www.xeno-canto.org, XC1100388", licenseLink: "https://creativecommons.org/licenses/by-nc-sa/4.0/", changes: "Original recording"}
+{english: "Rufous-naped Lark", afrikaans: "Rooineklewerik", hotspot: "Gauteng; KZN; Limpopo; Free State; Northwest; Northern Cape; Eastern Cape", habitat: "Grassland", birdgroup: "LBJs", level: "1 Beginner", 
+ audio: "LBJs/XC1100388_Rufous-napedLark.mp3", spectrogram: "LBJs/XC1100388_Rufous-napedLark.png",image: "LBJs/XC1100388_Rufous-napedLark.jpg", credit: "© Marna Buys, www.xeno-canto.org, XC1100388", licenseLink: "https://creativecommons.org/licenses/by-nc-sa/4.0/", changes: "Original recording"}
   
 ];
