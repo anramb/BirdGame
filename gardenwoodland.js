01c91d798e80a0e5ae3e5328e17989b5ca4bcff0
anramb <anramb@gmail.com>
2026-04-17 20:47:31 +0200

    Update gardenwoodland.js

--- gardenwoodland.js
+++ gardenwoodland.js
@@ -1,7 +1,7 @@
 const gardenwoodland = [
   
 {english: "Gorgeous Bushshrike", afrikaans: "Konkoit", hotspot: "St Lucia; Kruger National Park; Limpopo; KZN; Mpumalanga", habitat: "Woodland; Forest", birdgroup: "Garden and Woodland", level: "1 Beginner", 
- audio: "gardenwoodland/XC1100369_GorgeousBushshrike.mp3", spectrogram: "gardenwoodland/XC1100369_GorgeousBushshrike.png",image: "gardenwoodland/XC1100369_GorgeousBushshrike.jpg", credit: "© Marna Buys, www.xeno-canto.org, XC1100369, Background birds: emerald-spotted wood dove, grey-headed bushshrike, ring-necked dove, etc", licenseLink: "https://creativecommons.org/licenses/by-nc-sa/4.0/", changes: "Original recording"}
+ audio: "gardenwoodland/XC1100369_GorgeousBushshrike.mp3", spectrogram: "gardenwoodland/XC1100369_GorgeousBushshrike.png",image: "gardenwoodland/XC1100369_GorgeousBushshrike.jpg", credit: "© Marna Buys, www.xeno-canto.org, XC1100369, Background birds: emerald-spotted wood dove, grey-headed bushshrike, ring-necked dove, etc", licenseLink: "https://creativecommons.org/licenses/by-nc-sa/4.0/", changes: "Original recording"},
 
 {english: "Blue-mantled Crested Flycatcher", afrikaans: "Bloukuifvlieëvanger", hotspot: "Western Cape; Eastern Cape, KZN; Mpumalanga", habitat: "Forest", birdgroup: "Garden and Woodland", level: "3 Advanced", 
   audio: "gardenwoodland/XC1100676_Blue-mantledCrestedFlycatcher.mp3", spectrogram: "gardenwoodland/XC1100676_Blue-mantledCrestedFlycatcher.png",image: "gardenwoodland/XC1100676_Blue-mantledCrestedFlycatcher.jpg", credit: "© Marna Buys, www.xeno-canto.org, XC1100676", licenseLink: "https://creativecommons.org/licenses/by-nc-sa/4.0/", changes: "Original recording"},
