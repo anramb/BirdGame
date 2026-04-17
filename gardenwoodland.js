a79c89f74e39451e48c4bd14f4cd3ae9a538c7a7
anramb <anramb@gmail.com>
2026-04-17 21:15:59 +0200

    Update gardenwoodland.js

--- gardenwoodland.js
+++ gardenwoodland.js
@@ -6,7 +6,7 @@ const gardenwoodland = [
 {english: "Blue-mantled Crested Flycatcher", afrikaans: "Bloukuifvlieëvanger", hotspot: "Western Cape; Eastern Cape, KZN; Mpumalanga", habitat: "Forest", birdgroup: "Garden and Woodland", level: "3 Advanced", 
   audio: "gardenwoodland/XC1100676_Blue-mantledCrestedFlycatcher.mp3", spectrogram: "gardenwoodland/XC1100676_Blue-mantledCrestedFlycatcher.png",image: "gardenwoodland/XC1100676_Blue-mantledCrestedFlycatcher.jpg", credit: "© Marna Buys, www.xeno-canto.org, XC1100676", licenseLink: "https://creativecommons.org/licenses/by-nc-sa/4.0/", changes: "Original recording"},
 
-{english: "Woodward's Batis", afrikaans: "Woodwardbosbontrokkie", hotspot: "KZN; St Lucia", habitat: "Forest", birdgroup: "Garden and Woodland", level: "2 Intermediate", 
+{english: "Woodwards Batis", afrikaans: "Woodwardbosbontrokkie", hotspot: "KZN; St Lucia", habitat: "Forest", birdgroup: "Garden and Woodland", level: "2 Intermediate", 
 audio: "gardenwoodland/XC1100685_WoodwardsBatis.mp3", spectrogram: "gardenwoodland/XC1100685_WoodwardsBatis.png",image: "gardenwoodland/XC1100685_WoodwardsBatis.jpg", credit: "© Marna Buys, www.xeno-canto.org, XC1100685", licenseLink: "https://creativecommons.org/licenses/by-nc-sa/4.0/", changes: "Original recording"},
   
 ];
