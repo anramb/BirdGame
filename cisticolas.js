bfe12dc15ea3693440118bebe0f41738ae2e8e2c
anramb <anramb@gmail.com>
2026-04-17 19:49:35 +0200

    Update cisticolas.js

--- cisticolas.js
+++ cisticolas.js
@@ -3,8 +3,7 @@ const cisticolas = [
 {english: "Chirping Cisticola", afrikaans: "Piepende Tinktinkie", hotspot: "Botswana; Namibia", habitat: "Reedbeds", birdgroup: "Cisticolas", level: "3 Advanced", 
  audio: "cisticolas/XC339101_ChirpingCisticola.mp3", spectrogram: "cisticolas/XC339101_ChirpingCisticola.png", image: "cisticolas/XC339101_ChirpingCisticola.jpg", credit: "© Peter Boesman, www.xeno-canto.org, XC339101", licenseLink: "https://creativecommons.org/licenses/by-nc-nd/4.0/", changes: "Original recording"},
 
-{english: "Luapula Cisticola", afrikaans: "Luapula Tinktinkie", hotspot: "Botswana; Namibia", habitat: "Reedbeds", birdgroup: "Cisticolas", level: "3 Advanced", 
- audio: "cisticolas/XC978973_LuapulaCisticola.mp3", spectrogram: "cisticolas/XC978973_LuapulaCisticola.png",credit: "© Dries Van de Loock, www.xeno-canto.org, XC978973", licenseLink: "https://creativecommons.org/licenses/by-nc-nd/4.0/", changes: "Original recording"},
+
 
 {english: "Rattling Cisticola", afrikaans: "Bosveldtinktinkie", hotspot: "Gauteng; Limpopo; Mpumalanga; Free State, KZN; Northwest; Northern Cape", habitat: "Bushveld", birdgroup: "Cisticolas", level: "2 Intermediate", 
  audio: "cisticolas/XC1100387_RattlingCisticola.mp3", spectrogram: "cisticolas/XC1100387_RattlingCisticola.png",credit: "© Marna Buys, www.xeno-canto.org, XC1100387", licenseLink: "https://creativecommons.org/licenses/by-nc-sa/4.0/", changes: "Original recording"},
