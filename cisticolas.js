e056b504fa819a2697faa4528cb869cbc0a265d0
anramb <anramb@gmail.com>
2026-04-17 19:37:47 +0200

    Update cisticolas.js

--- cisticolas.js
+++ cisticolas.js
@@ -3,16 +3,12 @@ const cisticolas = [
 {english: "Chirping Cisticola", afrikaans: "Piepende Tinktinkie", hotspot: "Botswana; Namibia", habitat: "Reedbeds", birdgroup: "Cisticolas", level: "3 Advanced", 
  audio: "cisticolas/XC339101_ChirpingCisticola.mp3", spectrogram: "cisticolas/XC339101_ChirpingCisticola.png", image: "cisticolas/XC339101_ChirpingCisticola.jpg", credit: "© Peter Boesman, www.xeno-canto.org, XC339101", licenseLink: "https://creativecommons.org/licenses/by-nc-nd/4.0/", changes: "Original recording"},
 
-{english: "Common Whitethroat alarm call", afrikaans: "Witkeelsanger alarm call", hotspot: "Zaagkuildrift", habitat: "Bushveld", birdgroup: "Warblers", level: "3 Advanced", 
- audio: "warblers/XC521207_CommonWhitethroat.mp3", spectrogram: "warblers/XC521207_CommonWhitethroat.png",credit: "© Tony Archer, www.xeno-canto.org, XC521207, Alarm call, Background birds: red-faced mousebird", licenseLink: "https://creativecommons.org/licenses/by-nc-sa/4.0/", changes: "Original recording"},
+{english: "Luapula Cisticola", afrikaans: "Luapula Tinktinkie", hotspot: "Botswana; Namibia", habitat: "Reedbeds", birdgroup: "Cisticolas", level: "3 Advanced", 
+ audio: "cisticolas/XC978973_LuapulaCisticola.mp3", spectrogram: "cisticolas/XC978973_LuapulaCisticola.png",credit: "© Dries Van de Loock, www.xeno-canto.org, XC978973", licenseLink: "https://creativecommons.org/licenses/by-nc-nd/4.0/", changes: "Original recording"},
 
-{english: "Barred Wren-Warbler", afrikaans: "Gebande Sanger", hotspot: "Zaagkuildrift", habitat: "Bushveld", birdgroup: "Warblers", level: "2 Intermediate", 
- audio: "warblers/XC708257_BarredWren-Warbler.mp3", spectrogram: "warblers/XC708257_BarredWren-Warbler.png",credit: "© Tony Archer, www.xeno-canto.org, XC708257", licenseLink: "https://creativecommons.org/licenses/by-nc-sa/4.0/", changes: "Original recording"},
+{english: "Rattling Cisticola", afrikaans: "Bosveldtinktinkie", hotspot: "Gauteng; Limpopo; Mpumalanga; Free State, KZN; Northwest; Northern Cape", habitat: "Bushveld", birdgroup: "Cisticolas", level: "2 Intermediate", 
+ audio: "cisticolas/XC1100387_RattlingCisticola.mp3", spectrogram: "cisticolas/XC1100387_RattlingCisticola.png",credit: "© Marna Buys, www.xeno-canto.org, XC1100387", licenseLink: "https://creativecommons.org/licenses/by-nc-sa/4.0/", changes: "Original recording"},
 
-{english: "Rudd's Apalis", afrikaans: "Ruddse Kleinjantjie", hotspot: "St Lucia", habitat: "Forest", birdgroup: "Warblers", level: "3 Advanced", 
- audio: "warblers/XC62390_RuddsApalis.mp3", spectrogram: "warblers/XC62390_RuddsApalis.png",image: "warblers/XC62390_RuddsApalis.jpg", credit: "© Charles Hesse, www.xeno-canto.org, XC62390", licenseLink: "https://creativecommons.org/licenses/by-nc-nd/2.5/", changes: "Original recording"},
-  
-{english: "Marsh Warbler alarm call", afrikaans: "Europese Rietsanger alarm call", hotspot: "Zaagkuildrift", habitat: "Bushveld", birdgroup: "Warblers", level: "3 Advanced", 
-audio: "warblers/XC632299_MarshWarbler.mp3", spectrogram: "warblers/XC632299_MarshWarbler.png",credit: "© Tony Archer, www.xeno-canto.org, XC632299; Alarm call", licenseLink: "https://creativecommons.org/licenses/by-nc-sa/4.0/", changes: "Original recording"},
+
   
 ];
