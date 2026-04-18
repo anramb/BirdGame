93542143369a3c0191d18aa8fb119a742de780cc
anramb <anramb@gmail.com>
2026-04-18 15:43:57 +0200

    Add files via upload

--- seedeaters.js
+++ seedeaters.js
@@ -1,6 +1,52 @@
 const seedeaters = [
   
 {english: "Dark-backed Weaver", afrikaans: "Bosmusikant", hotspot: "KZN; Eastern Cape", habitat: "Forest", birdgroup: "Seedeaters", level: "2 Intermediate", 
- audio: "seedeaters/XC923579_Dark-backedWeaver.mp3", spectrogram: "seedeaters/XC923579_Dark-backedWeaver.png", image: "seedeaters/XC923579_Dark-backedWeaver.jpg", photographer: "Marna Buys", credit: "© Tim Cockrcroft, www.xeno-canto.org, XC923579", licenseLink: "https://creativecommons.org/licenses/by-nc-sa/4.0/", changes: "Original recording"}
+ audio: "seedeaters/XC923579_Dark-backedWeaver.mp3", spectrogram: "seedeaters/XC923579_Dark-backedWeaver.png", image: "seedeaters/XC923579_Dark-backedWeaver.jpg", photographer: "Marna Buys", credit: "© Tim Cockrcroft, www.xeno-canto.org, XC923579", licenseLink: "https://creativecommons.org/licenses/by-nc-sa/4.0/", changes: "Original recording"}, 
+
+{english: "Streaky-headed Seedeater", afrikaans: "Streepkopkanarie", hotspot: "Gauteng; Free State; KZN; Limpopo; Mpumalanga; Eatern Cape; Western Cape; Northwest", habitat: "Grassland", birdgroup: "Seedeaters", level: "2 Intermediate", 
+audio: "seedeaters/XC683134_Streaky-headedSeedeater.mp3", spectrogram: "seedeaters/XC683134_Streaky-headedSeedeater.png",credit: "© Lynette Rudman, www.xeno-canto.org, XC683134, Background birds: egyptian goose, neddicky", licenseLink: "https://creativecommons.org/licenses/by-nc-sa/4.0/", changes: "Original recording"},
+
+{english: "Yellow Canary", afrikaans: "Geelkanarie", hotspot: "Gauteng; Limpopo; Mpumalanga; Northwest; Western Cape; Eastern Cape; Northern Cape", habitat: "Grassland", birdgroup: "Seedeaters", level: "2 Intermediate", 
+audio: "seedeaters/XC709131_YellowCanary.mp3", spectrogram: "seedeaters/XC709131_YellowCanary.png",credit: "© Tony Archer, www.xeno-canto.org, XC709131", licenseLink: "https://creativecommons.org/licenses/by-nc-sa/4.0/", changes: "Original recording"},
+
+{english: "Yellow-fronted Canary", afrikaans: "Geeloogkanarie", hotspot: "Gauteng; Limpopo; Mpumalanga; Northwest; Eastern Cape; KZN", habitat: "Grassland", birdgroup: "Seedeaters", level: "1 Beginner", 
+audio: "seedeaters/XC280191_Yellow-frontedCanary.mp3", spectrogram: "seedeaters/XC280191_Yellow-frontedCanary.png",credit: "© Peter Boesman, www.xeno-canto.org, XC280191", licenseLink: "https://creativecommons.org/licenses/by-nc-nd/4.0/", changes: "Original recording"},
+
+{english: "Black-throated Canary", afrikaans: "Bergkanarie", hotspot: "Gauteng; Limpopo; Mpumalanga; Northwest; Karoo; Free State; Northern Cape", habitat: "Grassland", birdgroup: "Seedeaters", level: "2 Intermediate", 
+audio: "seedeaters/XC514721_Black-throatedCanary.mp3", spectrogram: "seedeaters/XC514721_Black-throatedCanary.png",credit: "© Tony Archer, www.xeno-canto.org, XC514721", licenseLink: "https://creativecommons.org/licenses/by-nc-sa/4.0/", changes: "Original recording"},
+
+{english: "Brimstone Canary", afrikaans: "Dikbekkanarie", hotspot: "KZN; Eastern Cape; Western Cape; Mpumalanga; Limpopo", habitat: "Grassland", birdgroup: "Seedeaters", level: "2 Intermediate", 
+audio: "seedeaters/XC674311_BrimstoneCanary.mp3", spectrogram: "seedeaters/XC674311_BrimstoneCanary.png",credit: "© Lynette Rudman, www.xeno-canto.org, XC674311", licenseLink: "https://creativecommons.org/licenses/by-nc-sa/4.0/", changes: "Original recording"},
+
+{english: "Cape Siskin", afrikaans: "Kaapse Pietjiekanarie", hotspot: "Western Cape", habitat: "Fynbos", birdgroup: "Seedeaters", level: "3 Advanced", 
+audio: "seedeaters/XC523735_CapeSiskin.mp3", spectrogram: "seedeaters/XC523735_CapeSiskin.png",credit: "© Frank Lambert, www.xeno-canto.org, XC523735", licenseLink: "https://creativecommons.org/licenses/by-nc-nd/4.0/", changes: "Original recording"},
+{english: "Drakensberg Siskin", afrikaans: "Drakensberg Pietjiekanarie", hotspot: "KZN; Free State; Lesotho", habitat: "Grassland", birdgroup: "Seedeaters", level: "3 Advanced", 
+audio: "seedeaters/XC505930_DrakensbergSiskin.mp3", spectrogram: "seedeaters/XC505930_DrakensbergSiskin.png",credit: "© Lynette Rudman, www.xeno-canto.org, XC505930", licenseLink: "https://creativecommons.org/licenses/by-nc-sa/4.0/", changes: "Original recording"},
+
+{english: "Forest Canary", afrikaans: "Gestreepte Kanarie", hotspot: "Eastern Cape; KZN; Western Cape; Free State; Mpumalanga; Limpopo", habitat: "Forest", birdgroup: "Seedeaters", level: "3 Advanced", 
+audio: "seedeaters/XC492040_ForestCanary.mp3", spectrogram: "seedeaters/XC492040_ForestCanary.png",credit: "© Tim Cockrcroft, www.xeno-canto.org, XC492040, Background birds: scaly-throated honeyguide", licenseLink: "https://creativecommons.org/licenses/by-nc-sa/4.0/", changes: "Original recording"},
+
+{english: "Protea Canary", afrikaans: "Witvlerkkanarie", hotspot: "Western Cape", habitat: "Fynbos", birdgroup: "Seedeaters", level: "3 Advanced", 
+audio: "seedeaters/XC515427_ProteaCanary.mp3", spectrogram: "seedeaters/XC515427_ProteaCanary.png",credit: "© Frank Lambert, www.xeno-canto.org, XC515427", licenseLink: "https://creativecommons.org/licenses/by-nc-nd/4.0/", changes: "Original recording"},
+
+{english: "White-throated Canary", afrikaans: "Witkeelkanarie", hotspot: "Eastern Cape; Free State; Northern Cape; Western Cape; Karoo", habitat: "Woodland", birdgroup: "Seedeaters", level: "3 Advanced", 
+audio: "seedeaters/XC727850_White-throatedCanary.mp3", spectrogram: "seedeaters/XC727850_White-throatedCanary.png",credit: "© Lynette Rudman, www.xeno-canto.org, XC727850", licenseLink: "https://creativecommons.org/licenses/by-nc-sa/4.0/", changes: "Original recording"},
+
+{english: "Black-headed Canary", afrikaans: "Swartkopkanarie", hotspot: "Northern Cape; Western Cape; Karoo; Free State; Lesotho", habitat: "Grassland", birdgroup: "Seedeaters", level: "2 Intermediate", 
+audio: "seedeaters/XC728099_Black-headedCanary.mp3", spectrogram: "seedeaters/XC728099_Black-headedCanary.png",credit: "© Lynette Rudman, www.xeno-canto.org, XC728099", licenseLink: "https://creativecommons.org/licenses/by-nc-sa/4.0/", changes: "Original recording"},
+
+{english: "Cape Canary", afrikaans: "Kaapse Kanarie", hotspot: "Eastern Cape; Free State; Mpumalanga; Limpopo; Western Cape; KZN; Northern Cape", habitat: "Grassland; Fynbos", birdgroup: "Seedeaters", level: "2 Intermediate", 
+audio: "seedeaters/XC515435_CapeCanary.mp3", spectrogram: "seedeaters/XC515435_CapeCanary.png",credit: "© Frank Lambert, www.xeno-canto.org, XC515435", licenseLink: "https://creativecommons.org/licenses/by-nc-nd/4.0/", changes: "Original recording"},
+
+{english: "Golden-breasted Bunting", afrikaans: "Rooirugstreepkoppie", hotspot: "Eastern Cape; KZN; Northern Cape; Gauteng; Mpumalanga; Limpopo", habitat: "Woodland", birdgroup: "Seedeaters", level: "2 Intermediate", 
+audio: "seedeaters/XC729132_Golden-breastedBunting.mp3", spectrogram: "seedeaters/XC729132_Golden-breastedBunting.png",credit: "© Tim Cockrcroft, www.xeno-canto.org, XC729132", licenseLink: "https://creativecommons.org/licenses/by-nc-sa/4.0/", changes: "Original recording"},
+
+{english: "Cape Bunting", afrikaans: "Rooivlerkstreepkoppie", hotspot: "Gauteng; Free State; Mpumalanga; Northern Cape; Karoo; Western Cape", habitat: "Woodland", birdgroup: "Seedeaters", level: "2 Intermediate", 
+audio: "seedeaters/XC744031_CapeBunting.mp3", spectrogram: "seedeaters/XC744031_CapeBunting.png",credit: "© Lynette Rudman, www.xeno-canto.org, XC744031", licenseLink: "https://creativecommons.org/licenses/by-nc-sa/4.0/", changes: "Original recording"},
+
+{english: "Cinnamon-breasted Bunting", afrikaans: "Klipstreepkoppie", hotspot: "Karoo; Free State; KZN; Gauteng; Northern Cape; Limpopo; Mpumalanga", habitat: "Woodland; Rocky", birdgroup: "Seedeaters", level: "2 Intermediate", 
+audio: "seedeaters/XC696636_Cinnamon-breastedBunting.mp3", spectrogram: "seedeaters/XC696636_Cinnamon-breastedBunting.png",credit: "© Tony Archer, www.xeno-canto.org, XC696636", licenseLink: "https://creativecommons.org/licenses/by-nc-sa/4.0/", changes: "Original recording"},
+
+{english: "Lark-like Bunting", afrikaans: "Vaalstreepkoppie", hotspot: "Free State; Northern Cape; Northwest; Western Cape", habitat: "Grassland", birdgroup: "Seedeaters", level: "2 Intermediate", audio: "seedeaters/XC392486_Lark-likeBunting.mp3", spectrogram: "seedeaters/XC392486_Lark-likeBunting.png",credit: "© Peter Boesman, www.xeno-canto.org, XC392486", licenseLink: "https://creativecommons.org/licenses/by-nc-nd/4.0/", changes: "Original recording"},
 
 ];
