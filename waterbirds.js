38fb73cce8e7e0028259258f4428b07d3fcef509
anramb <anramb@gmail.com>
2026-04-18 08:54:37 +0200

    Update waterbirds.js

--- waterbirds.js
+++ waterbirds.js
@@ -1,25 +1,24 @@
 const waterbirds = [
   
 { english: "Fulvous Whistling Duck", afrikaans: "Fluiteend", hotspot: "Zaagkuildrift", habitat: "Wetland", birdgroup: "Waterbirds", level: "2 Intermediate",
-  audio: "waterbirds/XC280235_FulvousWhistlingDuck.mp3", spectrogram: "waterbirds/XC280235_FulvousWhistlingDuck.png", credit: "© Peter Boesman, www.xeno-canto.org, XC280235", licenseLink: "https://creativecommons.org/licenses/by-nc-nd/4.0/", changes: "Original recording"},
+  audio: "waterbirds/XC280235_FulvousWhistlingDuck.mp3", spectrogram: "waterbirds/XC280235_FulvousWhistlingDuck.png", image: "waterbirds/XC280235_FulvousWhistlingDuck.jpg", credit: "© Peter Boesman, www.xeno-canto.org, XC280235", licenseLink: "https://creativecommons.org/licenses/by-nc-nd/4.0/", changes: "Original recording"},
   
 { english: "African Black Duck", afrikaans: "Swarteend", hotspot: "Marievale Bird Sanctuary; Zaagkuildrift", habitat: "Wetland", birdgroup: "Waterbirds", level: "1 Beginner",
- audio: "waterbirds/XC615647_AfricanBlackDuck.mp3", spectrogram: "waterbirds/XC615647_AfricanBlackDuck.png", credit: "© Nature sounds by Simply Birding, www.xeno-canto.org, XC615647", licenseLink: "https://creativecommons.org/licenses/by-nc-nd/4.0/", changes: "Original recording"},
+ audio: "waterbirds/XC615647_AfricanBlackDuck.mp3", spectrogram: "waterbirds/XC615647_AfricanBlackDuck.png", image: "waterbirds/XC615647_AfricanBlackDuck.jpg", credit: "© Nature sounds by Simply Birding, www.xeno-canto.org, XC615647", licenseLink: "https://creativecommons.org/licenses/by-nc-nd/4.0/", changes: "Original recording"},
 
 { english: "Spur-winged Goose",  afrikaans: "Wildemakou",  hotspot: "Marievale Bird Sanctuary; Zaagkuildrift", habitat: "Wetland", birdgroup: "Waterbirds", level: "1 Beginner",
-  audio: "waterbirds/XC126343_Spur-wingedGoose.mp3", spectrogram: "waterbirds/XC126343_Spur-wingedGoose.png", credit: "© Sander Bot, www.xeno-canto.org, XC126343", licenseLink: "https://creativecommons.org/licenses/by-nc-sa/3.0/", changes: "Original recording"},
+  audio: "waterbirds/XC126343_Spur-wingedGoose.mp3", spectrogram: "waterbirds/XC126343_Spur-wingedGoose.png", image: "waterbirds/XC126343_Spur-wingedGoose.jpg", credit: "© Sander Bot, www.xeno-canto.org, XC126343", licenseLink: "https://creativecommons.org/licenses/by-nc-sa/3.0/", changes: "Original recording"},
 
 {english: "Little Bittern", afrikaans: "Kleinrietreier", hotspot: "Marievale Bird Sanctuary", habitat: "Wetland", birdgroup: "Waterbirds", level: "3 Advanced",
- audio: "waterbirds/XC599094_LittleBittern.mp3", spectrogram: "waterbirds/XC599094_LittleBittern.png",credit: "© Tony Archer, www.xeno-canto.org, XC599094; Background birds: Lesser Swamp Warbler", licenseLink: "https://creativecommons.org/licenses/by-nc-sa/4.0/", changes: "Original recording"},
+ audio: "waterbirds/XC599094_LittleBittern.mp3", spectrogram: "waterbirds/XC599094_LittleBittern.png", image: "waterbirds/XC599094_LittleBittern.jpg", credit: "© Tony Archer, www.xeno-canto.org, XC599094; Background birds: Lesser Swamp Warbler", licenseLink: "https://creativecommons.org/licenses/by-nc-sa/4.0/", changes: "Original recording"},
 
 {english: "Dwarf Bittern", afrikaans: "Dwergrietreier", hotspot: "Zaagkuildrift", habitat: "Wetland", birdgroup: "Waterbirds", level: "3 Advanced",
- audio: "waterbirds/XC827385_DwarfBittern.mp3", spectrogram: "waterbirds/XC827385_DwarfBittern.png",credit: "© Ehren Eksteen, www.xeno-canto.org, XC827385; Background birds: Cape Turtle (RIng-necked) Dove, Woodland Kingfisher", licenseLink: "https://creativecommons.org/licenses/by-nc-sa/4.0/", changes: "Original recording"},
+ audio: "waterbirds/XC827385_DwarfBittern.mp3", spectrogram: "waterbirds/XC827385_DwarfBittern.png", image: "waterbirds/XC827385_DwarfBittern.jpg", credit: "© Ehren Eksteen, www.xeno-canto.org, XC827385; Background birds: Cape Turtle (RIng-necked) Dove, Woodland Kingfisher", licenseLink: "https://creativecommons.org/licenses/by-nc-sa/4.0/", changes: "Original recording"},
 
 {english: "Black-crowned Night Heron", afrikaans: "Gewone Nagreier", hotspot: "Zaagkuildrift", habitat: "Wetland", birdgroup: "Waterbirds", level: "3 Advanced",
- audio: "waterbirds/XC280512_Black-crownedNightHeron.mp3", spectrogram: "waterbirds/XC280512_Black-crownedNightHeron.png",credit: "© Peter Boesman, www.xeno-canto.org, XC280512", licenseLink: "https://creativecommons.org/licenses/by-nc-nd/4.0/", changes: "Original recording"},
+ audio: "waterbirds/XC280512_Black-crownedNightHeron.mp3", spectrogram: "waterbirds/XC280512_Black-crownedNightHeron.png", image: "waterbirds/XC280512_Black-crownedNightHeron.jpg", credit: "© Peter Boesman, www.xeno-canto.org, XC280512", licenseLink: "https://creativecommons.org/licenses/by-nc-nd/4.0/", changes: "Original recording"},
 
 {english: "Squacco Heron", afrikaans: "Ralreier", hotspot: "Zaagkuildrift", habitat: "Wetland", birdgroup: "Waterbirds", level: "3 Advanced",
- audio: "waterbirds/XC299963_SquaccoHeron.mp3", spectrogram: "waterbirds/XC299963_SquaccoHeron.png",credit: "© Peter Boesman, www.xeno-canto.org, XC299963", licenseLink: "https://creativecommons.org/licenses/by-nc-nd/4.0/", changes: "Original recording"},
-
+ audio: "waterbirds/XC299963_SquaccoHeron.mp3", spectrogram: "waterbirds/XC299963_SquaccoHeron.png", image: "waterbirds/XC299963_SquaccoHeron.jpg", credit: "© Peter Boesman, www.xeno-canto.org, XC299963", licenseLink: "https://creativecommons.org/licenses/by-nc-nd/4.0/", changes: "Original recording"}
 
 ];
