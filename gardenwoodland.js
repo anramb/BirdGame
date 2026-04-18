9ba9578467d6986b6732811f49c2cb6ec8427686
anramb <anramb@gmail.com>
2026-04-18 08:51:17 +0200

    Update gardenwoodland.js

--- gardenwoodland.js
+++ gardenwoodland.js
@@ -7,6 +7,6 @@ const gardenwoodland = [
   audio: "gardenwoodland/XC1100676_Blue-mantledCrestedFlycatcher.mp3", spectrogram: "gardenwoodland/XC1100676_Blue-mantledCrestedFlycatcher.png",image: "gardenwoodland/XC1100676_Blue-mantledCrestedFlycatcher.jpg", credit: "© Marna Buys, www.xeno-canto.org, XC1100676", licenseLink: "https://creativecommons.org/licenses/by-nc-sa/4.0/", changes: "Original recording"},
 
 {english: "Woodwards Batis", afrikaans: "Woodwardbosbontrokkie", hotspot: "KZN; St Lucia", habitat: "Forest", birdgroup: "Garden and Woodland", level: "2 Intermediate", 
-audio: "gardenwoodland/XC1100685_WoodwardsBatis.mp3", spectrogram: "gardenwoodland/XC1100685_WoodwardsBatis.png",image: "gardenwoodland/XC1100685_WoodwardsBatis.jpg", credit: "© Marna Buys, www.xeno-canto.org, XC1100685", licenseLink: "https://creativecommons.org/licenses/by-nc-sa/4.0/", changes: "Original recording"},
+audio: "gardenwoodland/XC1100685_WoodwardsBatis.mp3", spectrogram: "gardenwoodland/XC1100685_WoodwardsBatis.png",image: "gardenwoodland/XC1100685_WoodwardsBatis.jpg", credit: "© Marna Buys, www.xeno-canto.org, XC1100685", licenseLink: "https://creativecommons.org/licenses/by-nc-sa/4.0/", changes: "Original recording"}
   
 ];
