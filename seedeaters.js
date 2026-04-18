3e3b418bdd60a489b7890ee4c4da8827c34b9760
anramb <anramb@gmail.com>
2026-04-18 08:53:00 +0200

    Update seedeaters.js

--- seedeaters.js
+++ seedeaters.js
@@ -1,6 +1,6 @@
 const seedeaters = [
   
 {english: "Dark-backed Weaver", afrikaans: "Bosmusikant", hotspot: "KZN; Eastern Cape", habitat: "Forest", birdgroup: "Seedeaters", level: "2 Intermediate", 
- audio: "seedeaters/XC923579_Dark-backedWeaver.mp3", spectrogram: "seedeaters/XC923579_Dark-backedWeaver.png",image: "seedeaters/XC923579_Dark-backedWeaver.jpg", credit: "© Tim Cockrcroft, www.xeno-canto.org, XC923579", licenseLink: "https://creativecommons.org/licenses/by-nc-sa/4.0/", changes: "Original recording"}
+ audio: "seedeaters/XC923579_Dark-backedWeaver.mp3", spectrogram: "seedeaters/XC923579_Dark-backedWeaver.png", image: "seedeaters/XC923579_Dark-backedWeaver.jpg", credit: "© Tim Cockrcroft, www.xeno-canto.org, XC923579", licenseLink: "https://creativecommons.org/licenses/by-nc-sa/4.0/", changes: "Original recording"}
 
 ];
